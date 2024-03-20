fc828dfd82d939422a8620b19d79df8358b0188c
Masato Tsutsumi <masatotsutsumi@Sakos-Mac-Studio.local>
2024-03-20 16:08:54 +0900

    add origin

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from concrete.ml.torch.compile import compile_torch_model
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://fhe-playground.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
